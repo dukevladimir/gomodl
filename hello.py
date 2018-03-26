1d7e3c5012acd815f32ce34350e046454de372cd
Andy <andymarsden100@googlemail.com>
2018-03-26 20:49:35 +0100

    web-app

--- hello.py
+++ hello.py
@@ -3,19 +3,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def index():
-    return "Index!"
- 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
- 
-@app.route("/members")
-def members():
-    return "Members"
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
+    return render_template("index.html")
 
 @app.route("/signup")
 def signup():
