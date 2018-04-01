964b7b4d2528d685921b8747e10fe299e8d1c8a8
Andy <andymarsden100@googlemail.com>
2018-04-01 10:03:34 +0100

    core

--- js/core.py
+++ js/core.py
@@ -10,3 +10,16 @@ class UnaryOperation:
 	def output(self,value):
 		self.eval(value)
 		return self._output
+
+def BinaryOperation:
+
+	def __init__(self, func = None):
+		self._func = func
+		self._output = None
+
+	def eval(self,data):
+		self._output = self._func(data[0],data[1])
+
+	def output(self,data):
+		self.eval(data)
+		return self._output
